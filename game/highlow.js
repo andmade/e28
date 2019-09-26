1b830572c32e8bb9d83186bf2476ab1a2967e528
andmade <and@andmade.me>
2019-09-26 13:10:34 -0400

    Add an additional try
    Add comments to JS
    
    Correct spelling of high

--- game/highlow.js
+++ game/highlow.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+  /*   1. Generate the secret this.Number
+  2. Get the button elements
+  3. Get the element listing final results
+  4. Get the element listing final outcome
+  5. get the input element for guesses */
   const correctNumber = Math.floor(Math.random() * 100 + 1);
   const submitButton = document.querySelector(".submit-button");
   const resetButton = document.querySelector(".reset-button");
@@ -6,7 +11,7 @@ window.onload = function() {
   const endgameOutput = document.querySelector("#endgame_output");
   const guessInput = document.querySelector("#guess");
 
-  //Returns 0 if equal, 1 if guess is greater, -1 if guess is lower
+  /*   Returns 0 if equal, 1 if guess is greater, -1 if guess is lower*/
   function checkGuess(guess, actual) {
     if (guess == actual) {
       return 0;
@@ -16,9 +21,11 @@ window.onload = function() {
   }
 
   function runGame() {
+    // Get guess and remaining tries
     let guess = guessInput.value;
     let remainingTries = document.querySelector("#remaining_tries");
 
+    //Prep results output
     let currentGuessOutput = document.createElement("dt");
     currentGuessOutput.classList.add("past-guess");
     currentGuessOutput.textContent = guess;
@@ -26,10 +33,11 @@ window.onload = function() {
     let currentResultOutput = document.createElement("dd");
     currentResultOutput.classList.add("past-result");
 
+    //Check result and set output accordingly
     let result = checkGuess(guess, correctNumber);
     switch (result) {
       case 0:
-        currentResultOutput.textContent = "CORRECT";
+        currentResultOutput.textContent = "CORRECT!";
         currentResultOutput.classList.add("correct-result");
         break;
       case -1:
@@ -41,21 +49,28 @@ window.onload = function() {
         currentResultOutput.classList.add("incorrect-result");
         break;
     }
+
+    //Decrement tries, add result, clear the input field
     remainingTries.textContent--;
     guessesList.appendChild(currentGuessOutput);
     guessesList.appendChild(currentResultOutput);
     guessInput.value = "";
 
+    /* If the game has been won/no more tries, display final result and 
+    the reset button. Plus disable input if they lost. */
     if (result === 0) {
+      endgameOutput.classList.add("correct-result");
       endgameOutput.textContent = `You win! The correct number was ${correctNumber}.`;
       resetButton.style.display = "block";
     } else if (remainingTries.textContent == 0) {
+      endgameOutput.classList.add("incorrect-result");
       endgameOutput.textContent = `You lose! The correct number was ${correctNumber}.`;
       guessInput.disabled = true;
       resetButton.style.display = "block";
     }
   }
 
+  //mouse click listener
   submitButton.addEventListener("click", function(evt) {
     if (guessInput.value !== "") {
       evt.preventDefault();
@@ -63,6 +78,7 @@ window.onload = function() {
     }
   });
 
+  //enter key listener
   submitButton.addEventListener("keyup", function(evt) {
     if (event.keyCode == 13 && guessInput.value !== "") {
       evt.preventDefault();
@@ -70,6 +86,7 @@ window.onload = function() {
     }
   });
 
+  //reset click listener that simply reloads the page
   resetButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     window.location.reload();
