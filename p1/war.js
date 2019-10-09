84a8120fb0080c30548791bcdf527c5d2462031a
andmade <and@andmade.me>
2019-10-09 03:17:55 -0400

    Implement basic player card removal and replacement

--- p1/war.js
+++ p1/war.js
@@ -3,77 +3,29 @@ let app = new Vue({
   data: {
     playerSelection: null,
     playerDeck: [
-      {
-        value: 5,
-        suit: 'spades'
-      },
-      {
-        value: 11,
-        suit: 'diamonds'
-      },
-      {
-        value: 12,
-        suit: 'hearts'
-      },
-      {
-        value: 1,
-        suit: 'spades'
-      }
+      { value: 5, suit: 'spades' },
+      { value: 11, suit: 'diamonds' },
+      { value: 12, suit: 'hearts' },
+      { value: 1, suit: 'spades' }
     ],
     playerHand: [
-      {
-        value: 4,
-        suit: 'spades'
-      },
-      {
-        value: 10,
-        suit: 'diamonds'
-      },
-      {
-        value: 13,
-        suit: 'hearts'
-      },
-      {
-        value: 13,
-        suit: 'spades'
-      }
+      { value: 4, suit: 'spades' },
+      { value: 10, suit: 'diamonds' },
+      { value: 13, suit: 'hearts' },
+      { value: 13, suit: 'spades' }
     ],
     computerCard: null,
     computerDeck: [
-      {
-        value: 4,
-        suit: 'spades'
-      },
-      {
-        value: 10,
-        suit: 'diamonds'
-      },
-      {
-        value: 13,
-        suit: 'hearts'
-      },
-      {
-        value: 13,
-        suit: 'spades'
-      }
+      { value: 4, suit: 'spades' },
+      { value: 10, suit: 'diamonds' },
+      { value: 13, suit: 'hearts' },
+      { value: 13, suit: 'spades' }
     ],
     computerHand: [
-      {
-        value: 4,
-        suit: 'hearts'
-      },
-      {
-        value: 10,
-        suit: 'clubs'
-      },
-      {
-        value: 13,
-        suit: 'diamonds'
-      },
-      {
-        value: 13,
-        suit: 'spades'
-      }
+      { value: 4, suit: 'hearts' },
+      { value: 10, suit: 'clubs' },
+      { value: 13, suit: 'diamonds' },
+      { value: 13, suit: 'spades' }
     ],
     playerScore: 0,
     computerScore: 0,
@@ -98,6 +50,22 @@ let app = new Vue({
   computed: {
     playerCard: function() {
       return this.playerHand[this.playerSelection];
+    }
+  },
+  methods: {
+    playWar: function() {
+      if (!this.playerSelection && this.playerSelection !== 0) {
+        //TODO: Put in some sort of notice to player to pick a card first. For now, log.
+        console.log('No card selected');
+        return;
+      }
+      let newCard = this.playerDeck.shift();
+      if (newCard) {
+        this.playerHand.splice(this.playerSelection, 1, this.playerDeck.shift());
+      } else {
+        this.playerHand.splice(this.playerSelection, 1);
+      }
+      this.playerSelection = null;
     }
   }
 });
