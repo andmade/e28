67e7ac0e484364a7e40d1dd6aec6fda3cdc9a10e
andmade <and@andmade.me>
2019-10-17 22:07:59 -0400

    Submission Commit

--- p1/README.md
+++ p1/README.md
@@ -1,23 +1,29 @@
-## Project 1: War Card Game in Vue
+## Project 1: War Card Game (Modified) in Vue
 
 - By: Ashley Davis
 - Production URL: [http://p1.andmade.me](http://p1.andmade.me)
 
 ## Notes for instructor
 
-In completing the Week 4 assignment, I did some work converting my Week 2 High-Low game to a Vue version as I started going through the videos. However, since the Week 4 material goes over so much of the High-Low game in Vue, I decided it would be better to switch to a new game for Project 1. Therefore, I am switching my game to the **War** card game.
+Since the Week 4 material goes over so much of the High-Low game in Vue, I decided it would be better to switch to a new game for Project 1. Therefore, I switched to the **War** card game with some modifications.
+
+1. There is only 1 round.
+2. Players can choose from a hand of 5 cards.
+3. Scoring is such that:
+
+- Diamonds beat Spades and Spades beat Hearts
+- Hearts beat Clubs and Clubs beat Diamonds
+- If a two cards of the same color (i.e. Spades and Clubs, Hearts and Diamonds) are selected, the card with the highest number value wins. If the cards are the same value, it's a tie and neither side gains a point.
 
 ## Outside Resources
 
 For outside resources, do not include course notes or examples, but do include any/all of the following:
 
-- MDN Documentation Map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
-- CSS Entities: https://www.w3schools.com/cssref/css_entities.asp
-
-**Examples:**
-
-- External blogs/tutorials/books
-- Specific forum posts (e.g. StackOverflow) that provided information you utilized in your code
-- Sources for images that you did not create
-- Outside resources such as CSS frameworks, Google fonts, etc.
-- Anything else (if in doubt, cite!)
+- Image of Card Deck: http://clipart-library.com/clipart/8cxrbGE6i.htm
+- Javascript Map Prototype: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+- Shuffling the Card Deck / Randomizing a Javascript Array: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array and https://en.wikipedia.org/wiki/Fisher–Yates_shuffle _(This algorithm essentially seems to be the definitive/standard way to shuffle an array in an language that doesn't have a shuffle array function)_
+- CSS Entities: https://www.w3schools.com/cssref/css_entities.asp _(For generating the suit images)_
+- "Github: Call watch callback even when value is set to current value": https://github.com/vuejs/vue/issues/1302 _(Resulted in the roundCount property as a means of doing hand and deck updating after a round is played)._
+- "Lato" from Google Fonts: https://fonts.google.com/specimen/Lato
+- Normalize.css: https://necolas.github.io/normalize.css/
+- Little UI Details: https://codepen.io/tyrellrummage/pen/ZJPXgy _(for card hover/active effects)_
--- p1/war.js
+++ p1/war.js
@@ -1,66 +1,99 @@
-/* 
-1. implement computer pick
-2. implement battle function */
+let cardMapping = new Map([
+  [1, 'A'],
+  [2, '2'],
+  [3, '3'],
+  [4, '4'],
+  [5, '5'],
+  [6, '6'],
+  [7, '7'],
+  [8, '8'],
+  [9, '9'],
+  [10, '10'],
+  [11, 'J'],
+  [12, 'Q'],
+  [13, 'K']
+]);
+
+let generateDeck = function() {
+  let deck = [];
+  cardMapping.forEach(function(cardName, cardValue) {
+    ['spades', 'clubs', 'hearts', 'diamonds'].forEach(function(cardSuit) {
+      deck.push({ name: cardName, value: cardValue, suit: cardSuit });
+    });
+  });
+  return deck;
+};
 
 let app = new Vue({
   el: '#game',
   data: {
+    newGame: true,
+    showDirections: true,
+    deck: generateDeck(),
     playerSelection: null,
-    playerDeck: [
-      { name: '5', value: 5, suit: 'spades' },
-      { name: 'J', value: 11, suit: 'diamonds' },
-      { name: 'Q', value: 12, suit: 'hearts' },
-      { name: 'A', value: 1, suit: 'spades' }
-    ],
-    playerHand: [
-      { name: '4', value: 4, suit: 'spades' },
-      { name: '10', value: 10, suit: 'diamonds' },
-      { name: 'K', value: 13, suit: 'hearts' },
-      { name: 'K', value: 13, suit: 'spades' }
-    ],
-    computerCard: null,
-    computerDeck: [
-      { name: '4', value: 4, suit: 'spades' },
-      { name: '10', value: 10, suit: 'diamonds' },
-      { name: 'K', value: 13, suit: 'hearts' },
-      { name: 'K', value: 13, suit: 'spades' }
-    ],
-    computerHand: [
-      { name: '4', value: 4, suit: 'hearts' },
-      { name: '10', value: 10, suit: 'clubs' },
-      { name: 'K', value: 13, suit: 'diamonds' },
-      { name: 'K', value: 13, suit: 'clubs' }
-    ],
+    computerSelection: null,
+    playerDeck: [],
+    playerHand: [],
+    computerDeck: [],
+    computerHand: [],
     playerScore: 0,
     computerScore: 0,
     previousPlayerCard: null,
     previousComputerCard: null,
     roundCount: 1,
     roundResult: null,
-    gameResult: null
-  },
-  filters: {
-    cardString(card) {
-      return card.name + ' of ' + card.suit;
+    gameResult: null,
+    suitRanking: {
+      spades: { weak: 'diamonds', strong: 'hearts' },
+      clubs: { weak: 'hearts', strong: 'diamonds' },
+      diamonds: { weak: 'clubs', strong: 'spades' },
+      hearts: { weak: 'spades', strong: 'clubs' }
     }
   },
   computed: {
     playerCard: function() {
       return this.playerHand[this.playerSelection];
+    },
+    computerCard: function() {
+      return this.computerHand[this.computerSelection];
     }
   },
   watch: {
-    // Watcher which updates the players hand while the round results are calculated
     roundCount: function() {
       this.previousPlayerCard = this.playerCard;
       this.previousComputerCard = this.computerCard;
       this.updateHand(this.playerHand, this.playerSelection, this.playerDeck);
-      this.updateHand(this.computerHand, this.computerCard, this.computerDeck);
+      this.updateHand(this.computerHand, this.computerSelection, this.computerDeck);
       this.playerSelection = null;
-      this.computerCard = null;
+      this.computerSelection = null;
+
+      if (!this.playerHand.length) {
+        this.gameResult =
+          this.playerScore > this.computerScore
+            ? 'WIN'
+            : this.playerScore < this.computerScore
+            ? 'LOSE'
+            : 'TIE';
+      }
     }
   },
+  mounted: function() {
+    //Uncomment the below line to start the game automatically on load
+    //this.startGame();
+  },
+  // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
   methods: {
+    shuffleDeck: function() {
+      let newDeck = this.deck;
+      for (currentCard = 51; currentCard > 0; currentCard--) {
+        let newCard = Math.floor(Math.random() * (currentCard + 1));
+        let oldCard = newDeck[currentCard];
+        newDeck[currentCard] = newDeck[newCard];
+        newDeck[newCard] = oldCard;
+      }
+      return newDeck;
+    },
+
     updateHand: function(cardHand, selectedCard, cardDeck) {
       let newCard = cardDeck.shift();
       if (newCard != null) {
@@ -69,44 +102,66 @@ let app = new Vue({
         cardHand.splice(selectedCard, 1);
       }
     },
-    updatePlayerHand: function() {
-      // if (!this.playerSelection && this.playerSelection !== 0) {
-      //   //TODO: Put in some sort of notice to player to pick a card first. For now, log.
-      //   console.log('No card selected');
-      //   return;
-      // }
-      this.updateHand(this.playerHand, this.playerSelection, this.playerDeck);
-    },
 
     selectComputerCard: function() {
-      let computerSelection = Math.floor(Math.random() * (this.computerHand.length - 1));
-      console.log(computerSelection);
-      this.computerCard = this.computerHand[computerSelection];
-      console.log(this.computerCard.name + this.computerCard.suit);
+      this.computerSelection = Math.floor(Math.random() * (this.computerHand.length - 1));
     },
-    scoreRound: function() {
-      if (this.playerCard.value > this.computerCard.value) {
-        this.roundResult = 'win';
-        this.playerScore++;
-      } else if (this.playerCard.value < this.computerCard.value) {
-        this.roundResult = 'lose';
-        this.computerScore++;
+
+    rankCards: function(firstCard, secondCard) {
+      if (this.suitRanking[firstCard.suit].weak == secondCard.suit) {
+        return 'LOSE';
+      } else if (this.suitRanking[firstCard.suit].strong == secondCard.suit) {
+        return 'WIN';
       } else {
-        this.roundResult = 'tie';
+        return firstCard.value < secondCard.value
+          ? 'LOSE'
+          : firstCard.value > secondCard.value
+          ? 'WIN'
+          : 'TIE';
       }
-      console.log('player: ' + this.playerCard.value + ' ' + this.roundResult);
+    },
+
+    scoreRound: function() {
+      this.roundResult = this.rankCards(this.playerCard, this.computerCard);
+      switch (this.roundResult) {
+        case 'WIN':
+          this.playerScore++;
+          break;
+        case 'LOSE':
+          this.computerScore++;
+          break;
+      }
     },
 
     conductBattle: function() {
       this.selectComputerCard();
       this.scoreRound();
       this.roundCount++;
-      /* 
-      1. select and update computer card/hand/deck
-      2. compare score and return results
-      3. update player and computer scores
-      4. display the cards on screen
-      */
+    },
+
+    startGame: function() {
+      let newDeck = this.shuffleDeck();
+
+      this.playerDeck = newDeck.slice(0, 26);
+      this.computerDeck = newDeck.slice(26);
+
+      this.playerHand = this.playerDeck.splice(0, 5);
+      this.computerHand = this.computerDeck.splice(0, 5);
+
+      this.newGame = false;
+      this.showDirections = false;
+    },
+    replayGame: function() {
+      this.startGame();
+
+      this.previousPlayerCard = null;
+      this.previousComputerCard = null;
+
+      this.playerScore = 0;
+      this.computerScore = 0;
+
+      this.roundResult = null;
+      this.gameResult = null;
     }
   }
 });
