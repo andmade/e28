d80722542e5688cb226e62e15b8ac227ae8c4cdd
andmade <and@andmade.me>
2019-10-30 05:12:18 -0400

    Clean up commit

--- practice/practice.js
+++ practice/practice.js
@@ -1,8 +1,24 @@
-let app = new Vue({
-  el: '#app',
-  data: {
-    playerName: null,
-    guess: null,
-    guesses: []
+// Component Vue instance
+Vue.component('round-detail', {
+  // Note how the data property receives a function in a Component
+  data: function() {
+    return {
+      deleted: false
+    };
+  },
+  template: `
+  <div></div>
+
+<div></div>
+  `,
+  methods: {
+    deleteRound: function() {
+      this.deleted = true;
+    }
   }
+});
+
+// root Vue instance
+let app = new Vue({
+  el: '#app'
 });
---
+++ week5/script.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    answered: false,
+    solution: 35,
+    answer: ''
+  },
+  methods: {
+    submitAnswer: function() {}
+  }
+});
