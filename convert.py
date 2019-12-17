180b37651af8fce3fff5c0b8a82686ca5b016665
andmade <and@andmade.me>
2019-12-17 05:35:25 -0500

    Add rough draft of 2nd peer review

---
+++ convert.py
@@ -0,0 +1,41 @@
+import json
+import codecs
+
+converted_events = {}
+converted_events['events'] = []
+
+with open('gazette.json', 'r',encoding='utf-8-sig') as f:
+    events_dict = json.load(f)
+
+for event in events_dict:
+    current = {}
+    current['eventID'] = event['eventID']
+    current['title'] = event['title']
+    current['featured'] = False
+    current['description'] = event['description']
+    current['location'] = event['location']
+    current['link'] = event['webLink']
+    current['startDateTime'] = event['startDateTime']
+    current['endDateTime'] = event['endDateTime']
+    current['dateTimeFormatted'] = event['dateTimeFormatted']
+    current['allDay'] = event['allDay']
+    current['thumbnail'] = event['eventImage']['url']
+    current['largeImage'] = event['detailImage']['url']
+    current['cost'] = "Free"
+    current['sponsor'] = None
+    current['contactInfo'] = None
+
+    for field in event['customFields']:
+        if field['fieldID'] == 15202:
+            current['categories'] = field['value'].split(", ")
+        elif field['fieldID'] == 15227:
+            current['sponsor'] = field['value']
+        elif field['fieldID'] == 15228:
+            current['cost'] = field['value']
+        elif field['fieldID'] == 15225:
+            current['contactInfo'] = field['value']
+
+    converted_events['events'].append(current)
+
+with open('database.json', 'w') as outfile:
+    json.dump(converted_events,  outfile, indent=2, sort_keys=False,)
---
+++ peer-review2.md
@@ -0,0 +1,27 @@
+## P3 Peer Review
+
+- Reviewer's name: Ashley Davis
+- Reviewee's name: Indri Himawan
+- URL to reviewee's Github repo: _<https://github.com/ihimawan/e28_
+
+### Are you able to produce any errors or unexpected results?
+
+- able to choose select gender as a gender (and select gender not presented as default option)
+  <option disabled :value=null>Select Gender...</option>
+- new lines in input box not recognized in message output.
+- new message indicator doesn't change after read, only on refresh (acknowledged)
+- able to create blank looking for / why me
+
+### Were there any parts of the interface that you found confusing or unclear?
+
+- bit more separation of videos/links at the bottom
+
+### Are there aspects of the code that you feel were not self-evident and would benefit from comments?
+
+### Are there any parts of the code that you found interesting or taught you something new?
+
+### Are there any best practices discussed in course material that you feel were not addressed in the code?
+
+- condense some components
+
+### Do you have any additional comments not covered in the above questions?
